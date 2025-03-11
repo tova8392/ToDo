9d62aed070875483377c6965bf3b4e1fa8f63747
tova8392 <3158392t@gmail.com>
2025-03-11 16:40:24 +0200

    delete

--- Sarver/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ Sarver/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1014a77794c24d4ad491c0e134bdb9abaa8c230a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db9e7834e1885c2d293c02cf12e921a301a9cebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
