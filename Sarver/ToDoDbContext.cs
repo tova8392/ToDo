9d78659ab448598de1371620eda09dd444bf9e80
tova8392 <3158392t@gmail.com>
2025-03-18 15:50:49 +0200

    Update ToDoDbContext.cs

--- Sarver/ToDoDbContext.cs
+++ Sarver/ToDoDbContext.cs
@@ -17,7 +17,7 @@ public partial class ToDoDbContext : DbContext
     }
 
     public virtual DbSet<Item> Items { get; set; }
-    public virtual DbSet<User> Users { get; set; } // הוספת טבלת המשתמשים
+    //public virtual DbSet<User> Users { get; set; } // הוספת טבלת המשתמשים
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
